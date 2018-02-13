48b878ef761434c4947ff49cf4a53ecdaf08d28d
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-02-13 02:18:34 -0300

    maquetado de la pag. principal

--- src/pages/main/main.component.ts
+++ src/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'main-page',
   templateUrl: './main.component.html'
 })
 export class MainComponent {
-  
+
 }
